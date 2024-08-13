672a71fdd88c908ef9b0207ce5e364b7b024421a
alfonsspj <alfonsspj@gmail.com>
2024-08-13 01:42:16 -0300

    Implementación de jwt con cookies

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
-import { PORT } from './config.js';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+import { PORT, SECRET_JWT_KEY } from './config.js';
 import { UserRepository } from './user-repository.js';
 
 const app = express();
@@ -8,22 +10,36 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(express.json()); // middleware: transforma el cuerpo del body en json
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const token = req.cookies.access_token;
+    if (!token) return res.render('index');
+    
+    try {
+        const data = jwt.verify(token, SECRET_JWT_KEY);
+        res.render('index', data); // { _id, username }
+    } catch (error) {
+        res.render('index');    
+    }
 });
-
-app.get('/protected', (req, res) => {
-    // todo: if sesión del usuario
-    res.render('protected');
-    // todo: else 401
-})
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
     try {
         const user = await UserRepository.login({ username, password });
-        res.send({ user });
+        const token = jwt.sign(
+            { id: user._id, username: user.username }, 
+            SECRET_JWT_KEY,
+            { expiresIn: '1h'}
+        );
+        res.cookie('access_token', token, {
+                httpOnly: true, // la cookie solo se puede acceder en el servidor
+                secure: process.env.NODE_ENV === 'production', // solo se puede acceder en https
+                sameSite: 'strict', // solo se puede acceder en el mismo dominio
+                maxAge: 1000 * 60 * 60 // tiempo de validez de 1 hora
+            })
+           .send({ user, token });
     } catch (error) {
         res.status(401).send(error.message);
     }
@@ -44,7 +60,21 @@ app.post('/register', async (req, res) => {
 
 app.post('/logout', (req, res) => {});
 
-app.get('/protected', (req, res) => {});
+app.get('/protected', (req, res) => {
+    const token = req.cookies.access_token;
+    if (!token) {
+        return res.status(403).send('Access not authorized');
+    }
+    // todo: if sesión del usuario
+    try {
+        const data = jwt.verify(token, SECRET_JWT_KEY);
+        res.render('protected', data); // { _id, username }
+    } catch (error) {
+        return res.status(401).send('Access not authorized');
+    }
+    res.render('protected', { username: 'alfonso' });
+    // todo: else 401
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
